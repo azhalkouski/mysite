3f55baf7928d7ece96a0ca8d8aa2cf98312c4dbd
Artsiom Zhalkouski <azhalkouski@gmail.com>
2023-09-05 16:03:50 +0300

    implement html form for voting on polls

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 
 def index(request):
@@ -31,4 +32,34 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+        # request.POST['choice'] will raise KeyError if choice wasn't provided
+        # in POST data.
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        # The else clause is useful for code that must be executed if
+        # the try clause does not raise an exception. The use of the else
+        # clause is better than adding additional code to the try clause
+        # because it avoids accidentally catching an exception that wasn't
+        # raised by the code being protected by the try ... except statement.
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(
+            # reverse() in this case produces "/polls/3/results/",
+            # where the 3 is the value of question.id
+            reverse("polls:results", args=(question.id))
+        )
+    
