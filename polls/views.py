aac05fb5d56d14826f904078a11dba114b8911ae
Artsiom Zhalkouski <azhalkouski@gmail.com>
2023-09-08 13:02:19 +0300

    add tests for polls' DetailView

--- polls/views.py
+++ polls/views.py
@@ -23,11 +23,17 @@ class IndexView(generic.ListView):
 
 """
 generic.DetailView abstracts a concept of
-"Display a detail page for a partivular type of object."
+"Display a detail page for a particular type of object."
 """
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
